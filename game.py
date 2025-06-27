7248f8582ab85c26d7bc6b51e9e0ef6c041b4582
AlgoClassWork <kyxec.play@gmail.com>
2025-06-27 14:09:22 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,41 +1,61 @@
 from pygame import *
-# ИГРОВЫЕ НАСТРОЙКИ
+
+# -------------------- НАСТРОЙКИ ИГРЫ --------------------
 WINDOW_TITLE = 'Лабиринт'
 WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 500
 WINDOW_BACKGROUND = 'background.jpg'
-# Создание экрана
-window = display.set_mode( (WINDOW_WIDTH, WINDOW_HEIGHT) )
+
+# -------------------- ИНИЦИАЛИЗАЦИЯ ЭКРАНА --------------------
+window = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 display.set_caption(WINDOW_TITLE)
 background = transform.scale(image.load(WINDOW_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
-# Описание персонажей
-class GameSprite():
-    def __init__(self, x, y, width, height, speed, img):
+clock = time.Clock()
+
+# -------------------- КЛАССЫ --------------------
+class GameSprite:
+    def __init__(self, x, y, width, height, speed, img_path):
         self.cord_x = x
         self.cord_y = y
         self.speed = speed
-        self.image = transform.scale(image.load(img), (width, height))
+        self.image = transform.scale(image.load(img_path), (width, height))
+        self.rect = self.image.get_rect(topleft=(x, y))
 
     def show(self):
         window.blit(self.image, (self.cord_x, self.cord_y))
 
-player = GameSprite(x=0, y=400, width=100, height=100, speed=5, img='player.png')
-enemy = GameSprite(x=600, y=200, width=100, height=100, speed=5, img='enemy.png')
-goal = GameSprite(x=600, y=400, width=100, height=100, speed=5, img='goal.png')
+# -------------------- СОЗДАНИЕ СПРАЙТОВ --------------------
+player = GameSprite(x=20, y=400, width=50, height=50, speed=5, img_path='player.png')
+enemy = GameSprite(x=300, y=300, width=50, height=50, speed=3, img_path='enemy.png')
+goal = GameSprite(x=620, y=420, width=50, height=50, speed=0, img_path='goal.png')
 
-wall = GameSprite(x=100, y=100, width=20, height=300, speed=0 , img='wall.png')
+# -------------------- СОЗДАНИЕ СТЕНОК --------------------
+walls = [
+    GameSprite(100, 0, 20, 300, 0, 'wall.png'),   # Вертикальная стена слева
+    GameSprite(200, 200, 300, 20, 0, 'wall.png'), # Горизонтальная середина
+    GameSprite(600, 100, 20, 300, 0, 'wall.png'), # Правая вертикальная стена
+    GameSprite(300, 400, 200, 20, 0, 'wall.png'), # Нижняя правая стена
+    GameSprite(400, 100, 20, 100, 0, 'wall.png'), # Внутренняя вертикаль
+]
 
-# ИГРОВОЙ ЦИКЛ
+# -------------------- ОСНОВНОЙ ЦИКЛ --------------------
 while True:
-    # Обработка нажатия на крестик
+
+    # Обработка событий
     for some_event in event.get():
         if some_event.type == QUIT:
             exit()
-    # Отображение картинок
+
+    # Отображение фона
     window.blit(background, (0, 0))
+
+    # Отображение объектов
     player.show()
     enemy.show()
     goal.show()
-    wall.show()
-    # Обновление кадров
+    for wall in walls:
+        wall.show()
+
+    # Обновление экрана
     display.update()
+

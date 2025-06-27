8caa3140c9dfe828759199c12ff08d22a00e6639
AlgoClassWork <kyxec.play@gmail.com>
2025-06-27 15:14:42 +0600

    Update game.py

--- game.py
+++ game.py
@@ -26,19 +26,31 @@ class GameSprite(sprite.Sprite):
     def show(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+    def move(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 450:
+            self.rect.y += self.speed
+        if keys[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 650:
+            self.rect.x += self.speed
+
 # -------------------- СОЗДАНИЕ СПРАЙТОВ --------------------
 player = GameSprite(x=20, y=400, width=50, height=50, speed=5, img_path='player.png')
 enemy = GameSprite(x=300, y=300, width=50, height=50, speed=3, img_path='enemy.png')
 goal = GameSprite(x=620, y=420, width=50, height=50, speed=0, img_path='goal.png')
 
 # -------------------- СОЗДАНИЕ СТЕНОК --------------------
-walls = [
-    GameSprite(100, 0, 20, 300, 0, 'wall.png'),   # Вертикальная стена слева
-    GameSprite(200, 200, 300, 20, 0, 'wall.png'), # Горизонтальная середина
-    GameSprite(600, 100, 20, 300, 0, 'wall.png'), # Правая вертикальная стена
-    GameSprite(300, 400, 200, 20, 0, 'wall.png'), # Нижняя правая стена
-    GameSprite(400, 100, 20, 100, 0, 'wall.png'), # Внутренняя вертикаль
-]
+
+wall1 = GameSprite(100, 0, 20, 300, 0, 'wall.png')  
+wall2 = GameSprite(200, 200, 300, 20, 0, 'wall.png')
+wall3 = GameSprite(600, 100, 20, 300, 0, 'wall.png')
+wall4 = GameSprite(300, 400, 200, 20, 0, 'wall.png')
+wall5 = GameSprite(400, 100, 20, 100, 0, 'wall.png')
+
+walls = [ wall1, wall2, wall3, wall4, wall5 ]
 
 # -------------------- ОСНОВНОЙ ЦИКЛ --------------------
 while True:
@@ -59,15 +71,13 @@ while True:
         wall.show()
 
     # Движение обьектов
-    keys = key.get_pressed()
-    if keys[K_w] and player.rect.y > 0:
-        player.rect.y -= player.speed
-    if keys[K_s] and player.rect.y < 450:
-        player.rect.y += player.speed
-    if keys[K_a] and player.rect.x > 0:
-        player.rect.x -= player.speed
-    if keys[K_d] and player.rect.x < 650:
-        player.rect.x += player.speed
+    player.move()
+
+    # Проверка столкновений
+    for wall in walls:
+        if sprite.collide_rect(player, wall):
+            player.rect.x = 50
+            player.rect.y = 400
 
     # Обновление экрана
     display.update()
